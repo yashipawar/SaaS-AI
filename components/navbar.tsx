0dd6afe13194c2225c11637139fb04c62903cfad
Yashashri Pawar <120497906+yashipawar@users.noreply.github.com>
2023-08-25 16:30:45 +0530

    Add files via upload

---
+++ components/navbar.tsx
@@ -0,0 +1,14 @@
+import { UserButton } from "@clerk/nextjs";
+import MobileSidebar from "./mobile_sidebar";
+const Navbar = () => {
+    return (
+        <div className="flex items-center p-4">
+            <MobileSidebar />
+            <div className="flex w-full justify-end">
+                <UserButton afterSignOutUrl="/" />
+
+            </div>
+        </div>
+    );
+}
+export default Navbar;
